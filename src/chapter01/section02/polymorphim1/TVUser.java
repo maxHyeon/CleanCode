5f8b6cd168bc24cf3bac758be75919aec6e836f2
student <student@M13029>
2018-04-09 14:21:52 +0900

    다형성1. 다형성 없는 소스

---
+++ src/chapter01/section02/polymorphim1/TVUser.java
@@ -0,0 +1,11 @@
+package chapter01.section02.polymorphim1;
+
+public class TVUser {
+	public static void main(String[] args) {
+		LgTV tv = new LgTV();
+		tv.turnOn();
+		tv.turnOff();
+		tv.soundDown();
+		tv.soundUp();
+	}
+}
