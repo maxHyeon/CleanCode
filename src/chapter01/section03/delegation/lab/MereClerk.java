91f86dc8920ca6fe701f4da30510a9743048c788
student <student@M13029>
2018-04-10 09:11:11 +0900

    상속과 위임 day 2 초기 파일 커밋

---
+++ src/chapter01/section03/delegation/lab/MereClerk.java
@@ -0,0 +1,23 @@
+package chapter01.section03.delegation.lab;
+
+public class MereClerk {
+	String name;
+	double salary;
+
+	public MereClerk(String name, double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public double getSalary() {
+		return this.salary;
+	}
+
+	public void manageSalary(double rate) {
+		salary = salary + salary * (rate / 100);
+	}
+}
