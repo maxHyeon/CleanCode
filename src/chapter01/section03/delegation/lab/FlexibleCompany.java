c4b671d9307254a285670c33bd18db56c4e516cd
student <student@M13029>
2018-04-10 09:38:04 +0900

    Employee 클래스 상속 후 클라이언트 파일 변경

--- src/chapter01/section03/delegation/lab/FlexibleCompany.java
+++ src/chapter01/section03/delegation/lab/FlexibleCompany.java
@@ -1,31 +1,27 @@
 package chapter01.section03.delegation.lab;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlexibleCompany {
 	public static void main(String[] args) {
-		MereClerk mereClerk1 = new MereClerk("철수", 100);
-		MereClerk mereClerk2 = new MereClerk("영희", 100);
-		Manager manager = new Manager("홍길동", 200);
-		StudentWorker studentworker = new StudentWorker("Min", 60);
+		List<Employee> empList = new ArrayList<Employee>();
+		empList.add(new MereClerk("철수", 100));
+		empList.add(new MereClerk("영희", 100));
+		empList.add(new Manager("홍길동", 200));
+		empList.add(new StudentWorker("Min", 60));
 
 		System.out.println("현재 월급입니다.");
-		System.out.println(mereClerk1.getName() + "의 현재 월급은 " + mereClerk1.getSalary() + " 만원 입니다.");
-		System.out.println(mereClerk2.getName() + "의 현재 월급은 " + mereClerk2.getSalary() + " 만원 입니다.");
-		System.out.println(manager.getName() + "의 현재 월급은 " + manager.getSalary() + " 만원 입니다.");
-		System.out.println(studentworker.getName() + "의 현재 월급은 " + studentworker.getSalary() + " 만원 입니다.");
-
-		System.out.println("");
-
-		System.out.println("올린 후의 월급입니다.");
-		mereClerk1.manageSalary(10);
-		System.out.println(mereClerk1.getName() + "의 현재 월급은 " + mereClerk1.getSalary() + " 만원 입니다.");
-
-		mereClerk2.manageSalary(10);
-		System.out.println(mereClerk2.getName() + "의 현재 월급은 " + mereClerk2.getSalary() + " 만원 입니다.");
-
-		manager.manageSalary(10);
-		System.out.println(manager.getName() + "의 현재 월급은 " + manager.getSalary() + " 만원 입니다.");
 		
-		studentworker.manageSalary(10);
-		System.out.println(studentworker.getName() + "의 현재 월급은 " + studentworker.getSalary() + " 만원 입니다.");
+		for (Employee employee : empList) {
+			System.out.println(employee.getName() + "의 현재 월급은 " + employee.getSalary() + " 만원 입니다.");
+		}
+		
+		System.out.println("올린 후의 월급입니다.");
+		
+		for (Employee employee : empList) {
+			employee.manageSalary(10);
+			System.out.println(employee.getName() + "의 현재 월급은 " + employee.getSalary() + " 만원 입니다.");
+		}	
 	}
 }
